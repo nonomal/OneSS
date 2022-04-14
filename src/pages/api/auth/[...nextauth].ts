1b027c68ec7745dba5ec122ba51595f32639b8cf
Tualin14 <t@dzaaaaaa.com>
2022-04-14 10:59:16 +0800

    test

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,23 @@
 import NextAuth from "next-auth"
-import GitHubProvider from "next-auth/providers/github";
-import baseSetting from "@/setting/baseSetting";
+import CredentialsProvider from "next-auth/providers/credentials";
+import privateSecret from "@/setting/privateSecret";
 
 export default NextAuth({
     providers: [
-        GitHubProvider({
-            clientId: baseSetting.github.clientId,
-            clientSecret: baseSetting.github.clientSecret
-        })
+        CredentialsProvider({
+            name: " Account",
+            credentials: {
+                password: {label: "Password", type: "password"},
+            },
+            async authorize(credentials, {body}) {
+                const user = {name: "admin"}
+
+                if (body && body.password === privateSecret.token) {
+                    return user
+                } else {
+                    return null
+                }
+            },
+        }),
     ]
 })
