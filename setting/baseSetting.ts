528e0a41a8eef91836e42917f32b3e24c8cc6d7f
Tualin14 <t@dzaaaaaa.com>
2022-04-14 10:46:22 +0800

    test

--- setting/baseSetting.ts
+++ setting/baseSetting.ts
@@ -11,6 +11,10 @@ const baseSetting = {
         token_endpoint: `https://login.microsoftonline.com/${process.env.TENANT_ID}/oauth2/v2.0/token`,
         graph_endpoint: "https://graph.microsoft.com/v1.0"
     },
+    github:{
+        clientId: process.env.GITHUB_CLIENT_ID!,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET
+    }
 }
 
 export default baseSetting
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,23 +1,12 @@
 import NextAuth from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials";
-import privateSecret from "@/setting/privateSecret";
+import GitHubProvider from "next-auth/providers/github";
+import baseSetting from "@/setting/baseSetting";
 
 export default NextAuth({
     providers: [
-        CredentialsProvider({
-            name: " Account",
-            credentials: {
-                password: {label: "Password", type: "password"},
-            },
-            async authorize(credentials, {body}) {
-                const user = {name: 'admin'}
-
-                if (body && body.password === privateSecret.token) {
-                    return user
-                } else {
-                    return null
-                }
-            },
-        }),
+        GitHubProvider({
+            clientId: baseSetting.github.clientId,
+            clientSecret: baseSetting.github.clientSecret
+        })
     ]
 })
