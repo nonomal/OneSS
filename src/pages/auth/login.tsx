c8f6f75a6b489848bb55cb2d493082329fa2220a
Tualin14 <t@dzaaaaaa.com>
2022-04-17 10:40:22 +0800

    better ui

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,19 +1,23 @@
 import {GetServerSidePropsContext} from 'next'
 import {getCsrfToken} from "next-auth/react"
+import {useRouter} from "next/router";
+
 import Menu from "@/components/Menu/Menu";
 
 
 export default function Login({csrfToken}: { csrfToken: string }) {
+    const {error} = useRouter().query
     return (
         <>
             <Menu userName={"Login"}/>
 
-            <div className="w-full flex justify-center">
+            <div className="w-full flex flex-col items-center">
+                <div className={'w-8 h-24'}/>
                 <div className="card-body w-full max-w-xl">
                     <form className={"form-control"} method="post" action={"/api/auth/callback/credentials"}>
                         <input name="csrfToken" type="hidden" defaultValue={csrfToken}/>
                         <div className={"input-group"}>
-                            <input name="password" type="password" placeholder="Password" className="input input-bordered w-full"/>
+                            <input name="password" type="password" placeholder="Password" className={`input input-bordered w-full ${error && 'input-error'}`}/>
                             <button className="btn" type="submit">Login</button>
                         </div>
                     </form>
