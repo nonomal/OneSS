254cbccbee5f7206c4518a66a058abf5cbecb7e6
Tualin14 <t@dzaaaaaa.com>
2022-04-29 12:03:23 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['cdn.jsdelivr.net'],
+        domains: ['mystatic.dza.vin'],
     },
     reactStrictMode: true,
 }
